6b5e8b23f5d79f186d8a44075b362b0c13132755
Siyovushbek <siyovusht@gmail.com>
2022-11-12 09:18:59 +0300

    findChannel and addChannel methods covered with tests

--- src/test/java/me/siyovushbek/channels/channel/ChannelServiceTest.java
+++ src/test/java/me/siyovushbek/channels/channel/ChannelServiceTest.java
@@ -1,17 +1,25 @@
 package me.siyovushbek.channels.channel;
 
+import me.siyovushbek.channels.exception.ChannelNotFoundException;
+import me.siyovushbek.channels.exception.ChannelTitleAlreadyExistsException;
+import me.siyovushbek.channels.user.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
 class ChannelServiceTest {
 
     @Mock
     ChannelRepository repository;
 
+    @Captor
+    ArgumentCaptor<String> argumentCaptor;
     ChannelService underTest;
     AutoCloseable closeable;
 
@@ -27,11 +35,61 @@ class ChannelServiceTest {
     }
 
     @Test
-    void findChannelById() {
+    void itShouldReturnChannelFromDB() {
+        // given
+        String title = "myChannel";
+        Channel channelFromDb = new Channel(title, new User());
+        given(repository.findById(title)).willReturn(Optional.of(channelFromDb));
+
+        // when
+        Channel channel = underTest.findChannelById(title);
+
+        // then
+        Mockito.verify(repository).findById(argumentCaptor.capture());
+        assertThat(title).isEqualTo(argumentCaptor.getValue());
+        assertThat(channel).isEqualTo(channelFromDb);
+
     }
 
     @Test
-    void addChannel() {
+    void itShouldThrowErrorAsChannelDoesNotExist() {
+        // given
+        String title = "myChannel";
+        given(repository.findById(title)).willReturn(Optional.empty());
+
+        // when and then
+        assertThatThrownBy(() -> underTest.findChannelById(title))
+                .isInstanceOf(ChannelNotFoundException.class);
+
+        Mockito.verify(repository).findById(argumentCaptor.capture());
+        assertThat(title).isEqualTo(argumentCaptor.getValue());
+    }
+
+    @Test
+    void itShouldNotAddChannelAsItAlreadyExists() {
+        // given
+        ChannelRequest request = Mockito.mock(ChannelRequest.class);
+        User user = Mockito.mock(User.class);
+        given(request.getTitle()).willReturn("newChannel");
+        given(repository.existsById(request.getTitle())).willReturn(true);
+
+        // when && then
+        assertThatThrownBy(() -> underTest.addChannel(request, user))
+                .isInstanceOf(ChannelTitleAlreadyExistsException.class);
+        Mockito.verify(request).getTitle();
+
+    }
+
+    // TODO: complete all tests
+    @Test
+    void itShouldAddChannelToDB() {
+//        ChannelRequest request = Mockito.mock(ChannelRequest.class);
+//        User user = Mockito.mock(User.class);
+//        given(request.getTitle()).willReturn("newChannel");
+//        given(repository.existsById(request.getTitle())).willReturn(false);
+//        given(repository.save())
+//
+//        assertThat(underTest.addChannel(request, user).)
     }
 
     @Test
